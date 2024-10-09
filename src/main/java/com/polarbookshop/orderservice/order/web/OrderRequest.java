47e67ebed1457330608c4ec967791fa48b2c4a8f
DS0708 <jonum12312@gmail.com>
2024-10-09 14:21:48 +0900

    8 통합 테스트 및 JSON 계층 슬라이스 테스트 작성

--- src/main/java/com/polarbookshop/orderservice/order/web/OrderRequest.java
+++ src/main/java/com/polarbookshop/orderservice/order/web/OrderRequest.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.*;
 //사용자로부터 주문 요청이 들어오는 것에 대한 DTO이며 validation까지 진행
 public record OrderRequest(
         //null값을 가질 수 없고, 최소한 화이트 스페이스가 아닌 문자를 하나 이상 가져야 한다.
-        @NotBlank(message = "The book quantity must be defined.")
+        @NotBlank(message = "The book ISBN must be defined.")
         String isbn,
         //null값을 가질수 없고 값이 1~5 사이어야 함
         @NotNull(message = "The book quantity must be defined.")
