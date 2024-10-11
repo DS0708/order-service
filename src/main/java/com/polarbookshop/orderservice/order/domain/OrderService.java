9cb45a9163e6649c883cfff9041491190e54e0c2
DS0708 <jonum12312@gmail.com>
2024-10-11 13:35:57 +0900

    10.5 Spring Cloud Stream을 통한 메시지 생성 및 소비 -> 주석 수정

--- src/main/java/com/polarbookshop/orderservice/order/domain/OrderService.java
+++ src/main/java/com/polarbookshop/orderservice/order/domain/OrderService.java
@@ -42,6 +42,11 @@ public class OrderService {
                 .doOnNext(this::publishOrderAcceptedEvent); //주문이 접수되면 이벤트 발행
     }
     private void publishOrderAcceptedEvent(Order order){
+        /*
+        * RabbitMQ는 적어도 하나의 전달(at-least-one delivered)을 보증하기 때문에
+        * 중복으로 메시지를 받을 수 있음을 유념해야 한다.
+        * 따라서 연산이 멱등성(계속 실행해도 결과가 같음)을 가지도록 구현해야 한다.
+        * */
         if(!order.status().equals(OrderStatus.ACCEPTED)){
             return; //주문의 상태가 ACCEPTED가 아니면(REJECTED or DISPATCHED라면), 아무것도 하지 않음.
         }
@@ -49,6 +54,13 @@ public class OrderService {
         log.info("Sending order accepted event with id: {}", order.id());
         //메시지를 acceptOrder-out-0 바인딩에 명시적으로 보낸다. 실제 RabbitMQ에는 order-accepted라는 이름의 채널로 보내는 것
         var result = streamBridge.send("acceptOrder-out-0", orderAcceptedMessage);
+        /*
+        * 데이터의 발원지가 REST 엔드포인트이기 때문에 Spring Cloud Function에 등록할 수 있는 공급자 빈이 없고,
+        * 따라서 프레임워크가 RabbitMQ에 대해 필요한 바인딩을 생성할 수 있는 수단이 없다.
+        * 그래서 StreamBridge를 사용하여 'acceptOrder-out-0'라는 대상 바인딩에게 메시지를 보낸다.
+        * 그리고 acceptOrder라는 함수는 없지만 애플리케이션이 시작할 떄 Spring Cloud Stream은 내부적으로
+        * StreamBridge가 acceptOrder 바인딩을 통해 메시지를 발행하려는 것을 인식하고 이 바인딩 객체를 자동으로 생성한다.
+        * */
         log.info("Result of sending data for order with id {}; {}", order.id(), result);
     }
     //'order-dispatched'라는 채널을 구독하여 생기는 메시지에 대한 소비
